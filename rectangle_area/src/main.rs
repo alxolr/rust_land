36cba8e104fd47af817d381eb61d3945ce8b1b34
Alexandru Olaru <alxolr@gmail.com>
2019-04-15 22:48:38 +0300

    Played with methods and Structs

---
+++ rectangle_area/src/main.rs
@@ -0,0 +1,36 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 30,
+        height: 25,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 30,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Rect1 area {}", rect1.area());
+}
