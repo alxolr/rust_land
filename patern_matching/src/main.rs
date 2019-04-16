48a01323a6558a6ab1986196e379868548f3a795
Alexandru Olaru <alxolr@gmail.com>
2019-04-16 21:38:51 +0300

    Worked on pattern matching module

---
+++ patern_matching/src/main.rs
@@ -0,0 +1,22 @@
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let five = Some(5);
+    let _six = plus_one(five);
+    let _none = plus_one(None);
+    let some_u8_value = Some(3u8);
+
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+}
