967a49c1269e88f683a69fa2125b210199c35f25
Alexandru Olaru <alxolr@gmail.com>
2019-04-25 21:11:25 +0300

    Added the test case for failing

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,22 @@
+use lib::Config;
+use std::env;
+use std::process;
+
+mod lib;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = lib::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
