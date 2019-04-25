08fff391e723685450a8b540d66760a26e7403d1
Alexandru Olaru <alxolr@gmail.com>
2019-04-25 11:58:26 +0300

    Worked on cli minigrep

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,12 +1,55 @@
-fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyzzz";
+use std::fmt::Display;
 
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    let s: &'static str = "I have static lifetime.";
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
     if x.len() > y.len() {
         x
     } else {
