e495107e949c8d5a7981e5b2fec68d30124f1737
Alexandru Olaru <alxolr@gmail.com>
2019-04-30 17:59:20 +0300

    First minigrep program

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,22 +1,19 @@
+mod lib;
+
 use lib::Config;
 use std::env;
 use std::process;
-
-mod lib;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = lib::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
