b5c1fe02d5d25aed6e6de66253537b2323ec664e
Alexandru Olaru <alxolr@gmail.com>
2019-05-10 12:47:36 +0300

    Dived into basic mulithreading in rust

---
+++ multi_threading/src/main.rs
@@ -0,0 +1,72 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+mod mutex;
+
+fn main() {
+    mutex::other_main();
+    channel();
+    move_ownership();
+
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    handle.join().unwrap();
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+}
+
+fn move_ownership() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here is a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
+}
+
+fn channel() {
+    let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1))
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1))
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
